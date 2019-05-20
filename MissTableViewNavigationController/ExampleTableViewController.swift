879808f57728d54f0490d0a2e444eb634ead56e5
Liam Doughty <liam@loosestar.com>
2019-05-20 16:51:08 +1000

    Added CustomTableViewController

--- MissTableViewNavigationController/ExampleTableViewController.swift
+++ MissTableViewNavigationController/ExampleTableViewController.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+class ExampleTableViewController: UITableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Shortcut. Will eventually register a cell subclass which then be dequed
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "DefOink \(indexPath.row + 1)"
+        return cell
+    }
+}
