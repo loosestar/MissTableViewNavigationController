4c3767d3fbb3e67988fc8e51f8ad0ae3610de538
Liam Doughty <liam@loosestar.com>
2019-07-17 19:51:33 +1000

    Improved LS_DataDetailView on segue -- defined in LS_DataDetailViewController

--- MissTableViewNavigationController/LS_DataDetailView.swift
+++ MissTableViewNavigationController/LS_DataDetailView.swift
@@ -16,6 +16,24 @@ class LS_DataDetailView: UIView {
     var delegate: LS_BackButtonDelegate?
     var button = UIButton()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.blue
+        
+        let labelTest: UILabel = UILabel(frame: CGRect(x: 0.0, y: self.frame.height/2, width: self.frame.width, height: 30.0))
+        labelTest.backgroundColor = UIColor.white
+        labelTest.textAlignment = .center
+        labelTest.text = "blah blah blah"
+        labelTest.textColor = UIColor.white
+        
+        self.addSubview(labelTest)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func onBackButtonTouched(sender: UIButton) {
         // invoke the delegate when the button is touched
         delegate?.onBackButtonTouched(sender: sender)
